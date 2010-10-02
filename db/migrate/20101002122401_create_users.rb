ba1143e70bdef9b84aa2d172e7f6c037ae2871fe
Joao Azevedo <joao.c.azevedo@gmail.com>
2010-10-02 13:37:17 +0100

    added user model.

---
+++ db/migrate/20101002122401_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :username
+      t.string :name
+      t.string :hashed_password
+      t.string :salt
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
