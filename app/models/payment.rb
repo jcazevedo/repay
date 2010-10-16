f1790bb0850d299a8ea1453ba53d9f3ffa1eba78
Joao Azevedo <joao.c.azevedo@gmail.com>
2010-10-16 17:08:48 +0100

    added paid? method to payment and payment_component.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,10 @@
 class Payment < ActiveRecord::Base
   has_many :payment_components
+
+  def paid?
+    self.payment_components.each do |pc|
+      return false if !pc.paid?
+    end
+    return true
+  end
 end
--- app/models/payment_component.rb
+++ app/models/payment_component.rb
@@ -1,4 +1,8 @@
 class PaymentComponent < ActiveRecord::Base
   belongs_to :payment
   belongs_to :user
+
+  def paid?
+    paid >= value
+  end
 end
