293dde3499f65c3cb456ea6b0d48f58856fb54d4
Joao Azevedo <joao.c.azevedo@gmail.com>
2010-12-01 13:25:26 +0000

    updating payments is now possible.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   before_filter :check_filters, :load_users, :load_payments
 
   def check_filters
-    if !request.post?
+    if !request.post? && (params[:paid].nil? && params[:not_paid].nil?)
       params[:paid] = "true"
       params[:not_paid] = "true"
     end
@@ -33,8 +33,16 @@ class HomeController < ApplicationController
 
   def update_payment
     @payment = Payment.find(params[:id])
+
+    params[:payment_components].each do |id, val|
+      @pc = PaymentComponent.find(id)
+      @pc.update_attributes(val)
+    end
+
     if @payment.update_attributes(params[:payment])
-      redirect_to :action => 'index'
+      redirect_to :action => 'index', 
+                  :paid => params[:paid], 
+                  :not_paid => params[:not_paid]
     else
       render :action => 'index'
     end
--- app/models/payment_component.rb
+++ app/models/payment_component.rb
@@ -2,6 +2,9 @@ class PaymentComponent < ActiveRecord::Base
   belongs_to :payment
   belongs_to :user
 
+  validates_presence_of :paid, :value, :user_id
+  validates_numericality_of :paid, :value
+
   def paid?
     paid >= value
   end
