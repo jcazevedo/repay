a0f76242feac67931307032ff6148fccbc789361
Joao Azevedo <joao.c.azevedo@gmail.com>
2011-02-12 01:35:53 +0000

    added comments.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -16,29 +16,34 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  # Sets up a new session from a given user_id.
+  # Sets up a new UserSession from a given user_id.
   def set_up_user_session(user_id)
     session[:user_session] = UserSession.new(:user => User.find(user_id),
                                              :paid => false,
                                              :not_paid => true)
   end
 
+  # Deletes the current UserSession.
   def delete_user_session
     session[:user_session] = nil
   end
 
+  # Returns the currently logged in User.
   def current_user
     return session[:user_session].user if !session[:user_session].nil?
   end
 
+  # Defines whether or not paid Payments shall be loaded.
   def load_paid_payments?
     return session[:user_session].load_paid?
   end
 
+  # Defines whether or not not paid Payments shall be loaded.
   def load_not_paid_payments?
     return session[:user_session].load_not_paid?
   end
 
+  # Defines redirection based on the login status.
   def authorize
     unless user_logged_in?
       flash[:notice] = "Please log in"
@@ -46,6 +51,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  # Returns true if there is a user logged in, false otherwise.
   def user_logged_in?
     return !session[:user_session].nil? && !session[:user_session].user.nil?
   end
